296db36756b58f687e0ab6f61fba7f7a36dd2436
andersonaires-cc <andersonairesspp@hotmail.com>
2026-02-21 14:43:45 -0300

    Problem 1001 cpp

---
+++ problems/1001/solution.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+int main() {
+    
+    int number1 , number2;
+    int sum;
+
+    std:: cin >> number1;
+    std:: cin >> number2;
+
+    sum = number1 + number2;
+
+    std::cout << "X = " << sum << std::endl;
+
+    return 0;
+}
