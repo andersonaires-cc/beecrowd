afdad345074c68f28dd0d337dffb38ea25397bd5
andersonaires-cc <andersonairesspp@hotmail.com>
2026-02-21 14:56:57 -0300

    Files

--- problems/1001/cpp/solution.cpp
+++ problems/1001/cpp/solution.cpp
@@ -4,10 +4,8 @@ int main() {
     
     int number1 , number2;
     int sum;
-
     std:: cin >> number1;
     std:: cin >> number2;
-
     sum = number1 + number2;
 
     std::cout << "X = " << sum << std::endl;
